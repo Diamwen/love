c15689ad996411f096153a3efad5eb779c24b2ef
wenshuliang <wenshuliang@ngmm365.com>
2017-08-10 17:21:30 +0800

    搭建

---
+++ love-web/src/test/java/com/wensuling/LoveWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wensuling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoveWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
